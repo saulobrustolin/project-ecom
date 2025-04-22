54b6a802f253889554c5731fae5a6723c4d73db5
saulo <saulobrustolin@icloud.com>
2025-04-22 17:35:40 -0300

    upgrade header #4

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,16 @@ export interface ContentLoading {
 }
   
 export interface MouseOverProps extends ContentLoading {
-    onMouseContentEnter: () => void;
     onMouseContentLeave: () => void;
+    setMouseEnter: any;
+    setMouseEnterContent: any;
+}
+
+export interface ImagesContent {
+    images: {
+        res_768: string;
+        res_1280: string;
+        res_1664: string;
+      };
+    content: string;
 }
